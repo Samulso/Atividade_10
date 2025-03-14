bdd6ef9e5a467d79cb50a082f32c12f4619b810e
Samuel <sohfsamuelhenrique@gmail.com>
2025-03-14 13:20:02 -0300

    atividade 10

---
+++ pagCadastro_veiculos.js
@@ -0,0 +1,33 @@
+function cadastrarDados(event){
+
+    event.preventDefault();
+
+    const id = document.getElementById("id").value.trim();
+        if (!id) {
+            alert("Por favor, digite o ID antes de cadastrar!");
+            return;
+        }
+    const numero = document.getElementById("numero").value.trim();
+        if (!numero) {
+            alert("Por favor, digite o número antes de cadastrar!");
+            return;
+        }
+    const linha = document.getElementById("linha").value.trim();
+        if (!linha) {
+            alert("Por favor, digite a linha antes de cadastrar!");
+            return;
+        }
+
+        const veiculo = { id, numero, linha};
+
+    const dados = JSON.parse(localStorage.getItem("dados")) || [];
+
+    dados.push(veiculo);
+    
+
+    localStorage.setItem("dados", JSON.stringify(dados));
+    alert("Dados salvos!");
+    document.getElementById("id").value = "";
+    document.getElementById("numero").value = "";
+    document.getElementById("linha").value = "";
+}
